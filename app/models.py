ba927ee0262dcde9daf1f3f8d4755228eb45e4cf
Karlo Prukner <karloprukner05@gmail.com>
2026-07-28 20:20:32 +0100

    Updated base models and added user model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,15 @@
 from typing import List, Optional
-from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Integer, Boolean, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
 
+engine = create_engine('postgresql+psycopg2://test:test@localhost:5432/cinema')
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+class User(Base):
+    __tablename__ = 'users'
+    email: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean)
+
+Base.metadata.create_all(engine)
