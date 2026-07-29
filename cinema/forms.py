9675c155833b6d619f063f82c19ea5b7508cf272
Karlo Prukner <karloprukner05@gmail.com>
2026-07-29 22:14:51 +0100

    Added Regstration form and modified Login email form to email

--- cinema/forms.py
+++ cinema/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
+from cinema.models import User
 
 class LoginForm(FlaskForm):
-    email       = StringField('Email', validators=[DataRequired()])
+    email       = EmailField('Email', validators=[DataRequired()])
     password    = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit      = SubmitField('Submit')
+    submit      = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    email       = EmailField('Email', validators=[DataRequired()])
+    password    = PasswordField('Password', validators=[DataRequired()])
+    confirm_p   = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit      = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('The email is already registered. Please use another!')
+
+    def validate_password(self, password, confirm_p):
+        pass
