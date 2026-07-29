9aa0f5db1129965d43148019888ac767ddf0addd
Karlo Prukner <karloprukner05@gmail.com>
2026-07-29 14:48:40 +0100

    Added password hashing and compare functions methods

---
+++ cinema/models.py
@@ -0,0 +1,23 @@
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String, Integer, Boolean, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+engine = create_engine('postgresql+psycopg2://test:test@localhost:5432/cinema')
+class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+class User(UserMixin, Base):
+    __tablename__ = 'users'
+    email: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+Base.metadata.create_all(engine)
