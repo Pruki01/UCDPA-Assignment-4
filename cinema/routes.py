2786660a4ca21c605dee15b1c8a48d7daa3f743c
Karlo Prukner <karloprukner05@gmail.com>
2026-07-30 04:42:14 +0100

    Connected register view to database

--- cinema/routes.py
+++ cinema/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, flash, url_for
 from flask_login import current_user, login_user, logout_user
 from cinema.models import User
-from cinema.forms import LoginForm
+from cinema.forms import LoginForm, RegistrationForm
 from app import app, session
 
 @app.route('/')
@@ -33,6 +33,26 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('auth/login.html', title='Log In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        new_user = User(email=form.email.data)
+        new_user.set_password(form.password.data)
+        session.add(new_user)
+        session.commit()
+        return redirect(url_for('index'))
+
+    else:
+        print(form.errors)
+
+    return render_template ('auth/registration.html', title='Register', form=form)
 
 @app.route('/logout')
 def logout():
