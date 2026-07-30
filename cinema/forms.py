09a30c6053b5a8164f530b0cd0f7b81e6c0dd118
Karlo Prukner <karloprukner05@gmail.com>
2026-07-30 04:41:53 +0100

    Added equal_to validator to compare_p

--- cinema/forms.py
+++ cinema/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, EqualTo
 from cinema.models import User
+from app import session
 
 class LoginForm(FlaskForm):
     email       = EmailField('Email', validators=[DataRequired()])
@@ -12,13 +13,10 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     email       = EmailField('Email', validators=[DataRequired()])
     password    = PasswordField('Password', validators=[DataRequired()])
-    confirm_p   = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
+    confirm_p   = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match!')])
     submit      = SubmitField('Register')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = session.query(User).filter(User.email == email.data).first()
         if user is not None:
             raise ValidationError('The email is already registered. Please use another!')
-
-    def validate_password(self, password, confirm_p):
-        pass
