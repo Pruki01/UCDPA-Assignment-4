9c98d74e21e857b6b5932a6ddb033d06a456a783
Karlo Prukner <karloprukner05@gmail.com>
2026-07-30 04:48:52 +0100

    Fixed login password check

--- cinema/routes.py
+++ cinema/routes.py
@@ -11,19 +11,16 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print(current_user)
-    print(current_user.is_authenticated)
+
     if current_user.is_authenticated:
-        print('You are already logged in!')
         return redirect(url_for('index'))
 
     form = LoginForm()
     if form.validate_on_submit():
 
         user = session.query(User).filter(User.email == form.email.data).one()
-        print(user)
 
-        if user is None or user.check_password(form.password.data):
+        if user is None or not user.check_password(form.password.data):
 
             flash('Invalid username or password!')
             return redirect(url_for('login'))
@@ -31,6 +28,9 @@ def login():
 
         login_user(user, form.remember_me.data)
         return redirect(url_for('index'))
+
+    else:
+        print(form.errors)
 
     return render_template('auth/login.html', title='Log In', form=form)
 
