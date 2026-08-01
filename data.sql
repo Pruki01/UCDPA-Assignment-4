b43d0379c0107bc7241f03921ed490babe6aa9bb
Karlo Prukner <karloprukner05@gmail.com>
2026-08-01 19:44:37 +0100

    Added screenings queries

--- data.sql
+++ data.sql
@@ -86,3 +86,114 @@ INSERT INTO movies (title, genre, description, duration, status, image) VALUES
 	'PulpFiction.jpg'
 
 )
+
+-- Image: https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTcWb6EgdTNmBnimW_uauDxf0y1jsmmVlMmiboVZDd05ZlxnqnQhttps://www.instagram.com/p/DNDxGPXiZdn/
+INSERT INTO movies (title, genre, description, duration, status, image) VALUES
+(
+	'Spider-Man: Brand New Day',
+	'ACTION',
+	'A forgotten Peter Parker lives alone as a full-time Spider-Man until mounting pressure triggers a dangerous change and a powerful new enemy emerges.',
+	145,
+	'CURRENT',
+	'Spider-Man.jpg'
+
+)
+
+-- Image: https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQRK4B5d2j1ei8XiwWtOBo-HT0YHpyUqttdx_ls1d49Z-oIfGyv
+INSERT INTO movies (title, genre, description, duration, status, image) VALUES
+(
+	'The Odyssey (2026)',
+	'ACTION',
+	'After the Trojan War, Odysseus faces a dangerous voyage back to Ithaca, meeting creatures like the Cyclops Polyphemus, Sirens, and Calypso along the way.',
+	145,
+	'CURRENT',
+	'TheOdyssey.jpg'
+
+)
+
+-- Image: https://www.google.com/imgres?q=toy%20story%205&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F0%2F08%2FToy_Story_5_poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FToy_Story_5&docid=YC3X2EQvqcRbHM&tbnid=7wj7lk9RYLA6fM&vet=12ahUKEwi9ksGp-v-VAxXhVUEAHe1RLlQQnPAOegQIMhAA..i&w=260&h=385&hcb=2&ved=2ahUKEwi9ksGp-v-VAxXhVUEAHe1RLlQQnPAOegQIMhAA
+INSERT INTO movies (title, genre, description, duration, status, image) VALUES
+(
+	'Toy Story 5',
+	'DRAMA',
+	'Woody, Buzz, Jessie and the rest of the gang jobs are challenged when theyre introduced to electronics, a new threat to playtime.',
+	102,
+	'CURRENT',
+	'ToyStory5.jpg'
+
+)
+
+-- Image: https://www.google.com/imgres?q=evil%20dead%20burn&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BZTljMTZjZTItZjMzZi00NDBhLWFmMzAtOGNiNTlhY2MxMzg5XkEyXkFqcGc%40._V1_QL75_UX190_CR0%2C0%2C190%2C281_.jpg&imgrefurl=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt31170389%2F&docid=Zn5i8KwYZLrNLM&tbnid=LagY9Caw4Aby6M&vet=12ahUKEwj4oMz3-v-VAxVEUUEAHTUWEwEQnPAOegQIPxAA..i&w=190&h=281&hcb=2&itg=1&ved=2ahUKEwj4oMz3-v-VAxVEUUEAHTUWEwEQnPAOegQIPxAA
+INSERT INTO movies (title, genre, description, duration, status, image) VALUES
+(
+	'Evil Daed Burn',
+	'HORROR',
+	'After the loss of her husband, a woman seeks solace with her in-laws. As one by one they transform into deadites, she comes to discover that the vows she took in life - survive even in death.',
+	110,
+	'CURRENT',
+	'EvilDeadBurn.jpg'
+
+)
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	1,
+	'2026-10-23',
+	'19:30:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	1,
+	'2026-10-23',
+	'17:00:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	1,
+	'2026-10-23',
+	'14:00:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	1,
+	'2026-10-23',
+	'11:00:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	2,
+	'2026-10-23',
+	'20:45:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	2,
+	'2026-10-23',
+	'18:00:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	6,
+	'2026-10-24',
+	'14:10:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	6,
+	'2026-10-24',
+	'11:20:00'
+);
+
+INSERT INTO screenings (movie_id, screen_id, date, time) VALUES(
+	1,
+	9,
+	'2026-10-24',
+	'21:35:00'
+);
