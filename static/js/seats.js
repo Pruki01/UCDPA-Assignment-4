07aa642906d45f19fb0704211417432355adaa8c
Karlo Prukner <karloprukner05@gmail.com>
2026-08-01 20:29:54 +0100

    Modified getselected seats return

--- static/js/seats.js
+++ static/js/seats.js
@@ -1,26 +1,29 @@
 const seatsContainer    = document.querySelector('.seats');
 const seats             = document.querySelectorAll('.row .seat:not(.occupied):not(.empty)');
 const btn               = document.querySelector('.tickets-btn');
+const form              = document.querySelector('.booking-form');
+const selectedSeats     = document.querySelector('#tickets');
 
 console.log(seatsContainer);
 console.log(seats);
 
-const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 function getSelectedSeats(){
     
-    return [...document.querySelectorAll('.seat.selected')].map(seat => {
+    return_str = '';
+    
+    [...document.querySelectorAll('.seat.selected')].map(seat => {
         const row = seat.closest('.row');
 
         const rowIndex = [...document.querySelectorAll('.row')].indexOf(row);
         const seatIndex = [...row.children].indexOf(seat);
 
-        return {
-            row: rows[rowIndex],
-            seat: seatIndex
-        }
-    }
-    )
+        return_str += `${rows[rowIndex]}${seatIndex},`;
+
+    });
+
+    return return_str;
 }
 
 seatsContainer.addEventListener('click', e =>{
@@ -36,14 +39,8 @@ seatsContainer.addEventListener('click', e =>{
 
 });
 
-btn.addEventListener('click', () => {
-    console.log(getSelectedSeats());
+form.addEventListener('submit', () => {
 
-    fetch('/api/selected-seats', {
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(getSelectedSeats())
-    })
+    selectedSeats.value = getSelectedSeats();
+
 });
