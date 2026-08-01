d080c2400b4d207d267aba019e42186645be8ebf
Karlo Prukner <karloprukner05@gmail.com>
2026-08-01 05:37:56 +0100

    Added return seats function to button

--- static/js/seats.js
+++ static/js/seats.js
@@ -1,8 +1,27 @@
 const seatsContainer    = document.querySelector('.seats');
 const seats             = document.querySelectorAll('.row .seat:not(.occupied):not(.empty)');
+const btn               = document.querySelector('.tickets-btn');
 
 console.log(seatsContainer);
 console.log(seats);
+
+const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+function getSelectedSeats(){
+    
+    return [...document.querySelectorAll('.seat.selected')].map(seat => {
+        const row = seat.closest('.row');
+
+        const rowIndex = [...document.querySelectorAll('.row')].indexOf(row);
+        const seatIndex = [...row.children].indexOf(seat);
+
+        return {
+            row: rows[rowIndex],
+            seat: seatIndex
+        }
+    }
+    )
+}
 
 seatsContainer.addEventListener('click', e =>{
 
@@ -11,16 +30,20 @@ seatsContainer.addEventListener('click', e =>{
         !e.target.classList.contains('occupied') &&
         !e.target.classList.contains('empty')){
 
-        const seat = e.target;
-        const row = seat.closest('.row');
-        
-        const rowIndex = [...document.querySelectorAll('.row')].indexOf(row);
-        const seatIndex = [...row.children].indexOf(seat);
-
-        console.log(`Row: ${rowIndex}, Seat: ${seatIndex}`);
-
         e.target.classList.toggle('selected');
 
     }
 
+});
+
+btn.addEventListener('click', () => {
+    console.log(getSelectedSeats());
+
+    fetch('/api/selected-seats', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(getSelectedSeats())
+    })
 });
